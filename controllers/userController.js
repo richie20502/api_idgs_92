1516ce245fab74cf128fbfba7e884422d66a3c81
richie20502 <richie.lugo.recillas.1990@gmail.com>
2024-07-06 11:31:10 -0600

    clase 2024/07/06

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const user = require('../models/user');
 const User = require('../models/user');
 
 exports.register = async (req, res) => {
@@ -8,5 +9,14 @@ exports.register = async (req, res) => {
         res.status(200).send(user);
     } catch (error) {
         res.status(400).send(error.message);
+    }
+}
+
+exports.listUsers = async (req, res) => {
+    try {
+        const users = await User.find({}, '_id username email');
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(500).send('Error interno');
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/userController');
 /**
  * @swagger
  * /api/users/register:
- *   post:
+ *  post:
  *     summary: Register a new user
  *     tags: [Users]
  *     requestBody:
@@ -25,5 +25,32 @@ const userController = require('../controllers/userController');
  *         description: User registered successfully
  */
 router.post('/register', userController.register);
+
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: List all users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                   username:
+ *                     type: string
+ *                   email:
+ *                     type: string
+ *       500:
+ *         description: Internal Server Error
+ */
+router.get('/', userController.listUsers);
 
 module.exports = router;
